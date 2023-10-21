3b7e15b5fd0ebfced0c054a6d7e4e6e3a183b4f5
Khansa Rahim <khansarahimm@gmail.com>
2023-10-21 10:07:42 -0700

    Adding Task1-Easy-HelloWorld

---
+++ C++/Easy/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+//Khansa Rahim
+//https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
